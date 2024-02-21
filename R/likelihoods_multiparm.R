e4bd495963264a5b72ffab19c3cd9658ad295a3c
Jake <dr.jake.carson@gmail.com>
2024-02-21 10:51:27 +0000

    Start on new ttree likelihood

--- R/likelihoods_multiparm.R
+++ R/likelihoods_multiparm.R
@@ -74,7 +74,6 @@ dyn_U <- function(ttree, grid, omega, w.shape, w.scale, obs.end, grid.delta = 1 
   inf_time1 <- ttree[host1, 1]
   inf_time2 <- ttree[host2, 1]
 
-  tidx1 <- 1 + floor((obs.end + 1e-10 - inf_time1) / grid.delta)
   tidx2 <- 1 + floor((obs.end + 1e-10 - inf_time2) / grid.delta)
 
   omega_int2 <- omega[tidx2, deme2] + ((inf_time2 - grid[tidx2]) / (grid[tidx2 + 1] - grid[tidx2])) * (omega[tidx2 + 1, deme2] - omega[tidx2, deme2])
@@ -82,5 +81,92 @@ dyn_U <- function(ttree, grid, omega, w.shape, w.scale, obs.end, grid.delta = 1 
   Uns <- (1 - omega_int2) * dgamma(inf_time2 - inf_time1, shape = w.shape, scale = w.scale) * pm[deme1, deme2]
 
   return(Uns)
+
+}
+
+
+
+#' Calculate transmission tree likelihood
+#'
+#' @param ttree Transmission tree
+#' @param grid Discrete grid over which to evaluate functions
+#' @param fn_list Precalculated discrete approximations of exclusion probabilities
+#' @param off.r Shape parameter for the number of offspring
+#' @param off.p Probability parameter for the number of offspring
+#' @param pi Probability of host being sampled
+#' @param w.shape Shape parameter of generation time distribution
+#' @param w.scale Scale parameter of generation time distribution
+#' @param ws.shape Shape parameter of primary sampling time distribution
+#' @param ws.scale Scale parameter of primary sampling time distribution
+#' @param obs.start Start time of outbreak sampling
+#' @param obs.end Stop time of outbreak sampling
+#' @param grid.delta Discrete time step
+#' @param hosts Hosts over which likelihood is calculated
+log_lik_ttree_multiparm <- function(ttree, grid, fn_list, off.r, off.p, pi, w.shape, w.scale, ws.shape,
+                          ws.scale, obs.start, obs.end, grid.delta = 1 / 365, ndemes) {
+
+
+  obs <- ttree$obs
+  demes <- ttree$demes
+  ttree <- ttree$ttree
+
+  omega <- fn_list$omega
+  omega_bar <- fn_list$omega_bar
+  pit <- fn_list$pit
+  gamma_prob <- fn_list$gamma_prob
+
+  nhosts <- length(ttree[, 1])
+
+  dyn_L <- array(0, dim = c(nhosts, ndemes))
+
+  host_order <- order(ttree[, 1], decreasing = T)
+
+  for (i in host_order) {
+
+    # Leaf
+    if (length(which(ttree[, 3] == i)) == 0) {
+
+      if (!is.na(demes[i])) {
+
+        if (demes[i] > 0) {
+
+          dyn_L[i, demes[i]] <- dyn_T(ttree, obs, grid, omega, omega_bar, pit, off.r, off.p, pi, ws.shape,
+                                      ws.scale, obs.start, obs.end, grid.delta, i, demes[i])
+
+        } else {
+
+          for (d in 1:ndemes) {
+
+            dyn_L[i, d] <- dyn_T(ttree, obs, grid, omega, omega_bar, pit, off.r, off.p, pi, ws.shape,
+                                        ws.scale, obs.start, obs.end, grid.delta, i, d)
+
+          }
+
+        }
+
+      } else {
+
+        for (d in 1:ndemes) {
+
+          dyn_L[i, d] <- dyn_T(ttree, obs, grid, omega, omega_bar, pit, off.r, off.p, pi, ws.shape,
+                               ws.scale, obs.start, obs.end, grid.delta, i, d)
+
+        }
+
+      }
+
+    } else {
+
+
+
+    }
+
+  }
+
+
+
+
+
+
 
 }
